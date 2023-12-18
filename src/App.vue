cde7f951ac494e62a412eaa5504c482e8e20fea0
xiaoshu <2376628285@qq.com>
2023-12-18 16:15:56 +0800

    项目入库

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-button type="primary">使用element-plus的第一个按钮</el-button>
+    <el-icon>
+      <Edit />
+    </el-icon>
+    <div class="box">scss</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Edit } from '@element-plus/icons-vue'
+</script>
+
+<style scoped lang="scss">
+.box{
+  width: 100px;
+  height: 100px;
+  background-color: $base-background;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
+// 配置@别名
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  // 相对路径别名配置 @
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
+    },
+  },
+  // scss 全局变量配置
+  css: {
+    preprocessorOptions: {
+      scss: {
+        javascriptEnabled: true,
+        additionalData: '@import "./src/styles/variable.scss";',
+      },
+    },
+  },
+});
