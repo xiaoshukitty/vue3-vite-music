ee228c7e900d0cdcb00e2b5cf447df7d973b3bd3
xiaoshu <2376628285@qq.com>
2023-12-27 16:10:51 +0800

    fix:update

--- src/view/play/albums/index.vue
+++ src/view/play/albums/index.vue
@@ -4,7 +4,35 @@
     </div>
     <el-tabs v-model="activeName" class="demo-tabs" v-if="albumData">
         <el-tab-pane :label="`歌曲 ${num}`" name="song">
-            <SongList :songsList="songsList" :id="id"></SongList>
+            <el-row class="fs14 mt20">
+                <el-col :span="14">歌曲</el-col>
+                <el-col :span="8">专辑</el-col>
+                <el-col :span="2">时长</el-col>
+            </el-row>
+            <div class="song_list">
+                <el-row class="h50 song_info" v-for="(item, index) in songsList" :key="index" @dblclick="playMusic(item.id)"
+                    :class="{ 'playing': id === item.id }">
+                    <el-col class="music_song lh50" :span="14">
+                        <div class="df">
+                            <div class="music_mv">
+                                <div>{{ item.name }}</div>
+                                <div style="display: flex; margin-left:5px;">
+                                    <el-icon class="mv_hover" :size="20" color="#000" @click.stop="playMv(item.id)">
+                                        <VideoCamera />
+                                    </el-icon>
+                                </div>
+                            </div>
+                            <div style="display: flex; align-items: center;">
+                                <el-icon class="icon" :size="20" color="#fff" @click.stop="playMusic(item.id)">
+                                    <CaretRight />
+                                </el-icon>
+                            </div>
+                        </div>
+                    </el-col>
+                    <el-col class="collection lh50" :span="8">{{ item.al.name }}</el-col>
+                    <el-col class="duration lh50" :span="2">{{ convertToHMS(item.dt / 1000) }}</el-col>
+                </el-row>
+            </div>
         </el-tab-pane>
         <el-tab-pane label="评论" name="review">review</el-tab-pane>
         <el-tab-pane label="专辑详情" name="albumDetails">albumDetails</el-tab-pane>
@@ -13,12 +41,17 @@
 
 <script setup lang='ts'>
 import SongInfoHead from '@/components/common/SongInfoHead/index.vue'
-import SongList from '@/components/common/SongList/index.vue'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useAlbum } from '@/api/index'
 import type { Album } from '@/api/types/album'
 import { Song } from "@/api/types/index";
+import {
+    CaretRight,
+    VideoCamera
+} from '@element-plus/icons-vue'
+import { convertToHMS } from '@/utils/index'
+import { usePlaySong } from "@/store/modules/playSong";
 
 const albumData = ref<Album>()
 const songsList = ref<Song[]>([])
@@ -34,9 +67,89 @@ onMounted(async () => {
     num.value = songs.length;
 
 })
+const { playMusic } = usePlaySong()
+//跳转到 mv 页面
+const playMv = (id: number) => {
+    console.log('id---', id);
+}
 </script>
 <style scoped lang="scss">
 ::v-deep .el-tabs__nav-wrap::after {
     height: 0;
+}
+
+.song_list {
+    .song_info {
+        font-size: 14px;
+        cursor: pointer;
+
+        .lh50 {
+            line-height: 50px;
+        }
+
+        .music_song {
+            .icon {
+                display: none;
+            }
+
+            .icon:hover {
+                color: $base-background;
+            }
+
+            .music_mv {
+                display: flex;
+                align-items: center;
+                margin-left: 5px;
+
+                .mv_hover:hover {
+                    color: $base-background;
+                }
+            }
+        }
+
+        .collection {}
+
+        .duration {}
+    }
+
+    .song_info:hover {
+        --tw-bg-opacity: 1;
+        background-color: rgb(229 231 235 / var(--tw-bg-opacity));
+    }
+
+    .song_info:hover .icon {
+        display: inline;
+        color: silver;
+    }
+}
+
+.loading_btn {
+    margin-top: 10px;
+    text-align: center;
+}
+
+.fs14 {
+    font-size: 14px;
+    color: silver;
+}
+
+.mt20 {
+    margin-top: 20px;
+    margin-bottom: 10px;
+}
+
+.h50 {
+    height: 50px;
+}
+
+.playing {
+    --tw-bg-opacity: 1;
+    background-color: rgb(236 253 245 / var(--tw-bg-opacity));
+}
+
+.df {
+    display: flex;
+    justify-content: space-between;
+    padding-right: 20px;
 }
 </style>
