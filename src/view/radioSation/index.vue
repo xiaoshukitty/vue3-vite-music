f05f4c9520b412f9df0ea3a5773b958fcde237b5
xiaoshu <2376628285@qq.com>
2024-08-17 19:53:46 +0800

    feat(radioSation):add Skeleton screen

--- src/view/radioSation/index.vue
+++ src/view/radioSation/index.vue
@@ -1,49 +1,88 @@
 <template>
     <div class="radioSation" v-loading="loading">
-        <div class="song-top">
-            <div class="song-img">
-                <img :src="zeroObj.coverImgUrl" alt="">
-            </div>
-            <div class="song-right">
-                <div class="song-txt">
-                    <span>{{ songType }}</span>
-                </div>
-                <div class="song-txt1">{{ zeroObj.name }}</div>
-                <div class="song-txt2">{{ zeroObj.description }}</div>
-            </div>
-        </div>
-        <div class="song-center">
-            <el-scrollbar :class="isBg ? 'tabs_bg1' : 'tabs_bg'">
-                <div class="scrollbar-flex-content">
-                    <p v-for="(item, index) in tagList" :key="item"
-                        :class="['scrollbar-demo-item', avatarIndex == index ? 'active' : '']"
-                        @click="binTag(index, item)">
-                        {{ item.name }}
-                    </p>
-                </div>
-            </el-scrollbar>
-        </div>
-        <div class="song-bottom">
-            <div class="song-bottom-list">
-                <div class="song-bottom-item" v-for="(item, index) in songList.playlists" :key="item.id">
-                    <div class="block_item" @click="goToPlayList(item)">
-                        <PlaylistModule :imgSrc="item.coverImgUrl"></PlaylistModule>
+        <el-space style="width: 100%" fill>
+            <el-skeleton class="skelton" animated :loading="skeletonLoading">
+                <template #template>
+                    <div class="skelton-img">
+                        <el-skeleton-item variant="image" class="skelton-img2" />
+                        <div class="skelton-img1">
+                            <el-skeleton-item variant="h3" style="width: 100%; margin-top: 5px;" />
+                            <el-skeleton-item variant="h3" style="width: 100%; margin-top: 5px;" />
+                            <el-skeleton-item variant="h3" style="width: 100%; margin-top: 5px;" />
+                        </div>
                     </div>
-                    <div class="text">{{ item.name }}</div>
-                </div>
+                </template>
+                <template #default>
+                    <div class="song-top">
+                        <div class="song-img">
+                            <img :src="zeroObj.coverImgUrl" alt="">
+                        </div>
+                        <div class="song-right">
+                            <div class="song-txt">
+                                <span>{{ songType }}</span>
+                            </div>
+                            <div class="song-txt1">{{ zeroObj.name }}</div>
+                            <div class="song-txt2">{{ zeroObj.description }}</div>
+                        </div>
+                    </div>
+                </template>
+            </el-skeleton>
+            <el-skeleton class="skelton" animated :count="1" :loading="skeletonLoading">
+                <template #template>
+                    <div class="skelton-box">
+                        <div v-for="(item, index) in 40" :key="index">
+                            <el-skeleton-item variant="h3" class="skelton-box-item" />
+                        </div>
+                    </div>
+                </template>
+                <template #default>
+                    <div class="song-center">
+                        <el-scrollbar :class="isBg ? 'tabs_bg1' : 'tabs_bg'">
+                            <div class="scrollbar-flex-content">
+                                <p v-for="(item, index) in tagList" :key="item"
+                                    :class="['scrollbar-demo-item', avatarIndex == index ? 'active' : '']"
+                                    @click="binTag(index, item)">
+                                    {{ item.name }}
+                                </p>
+                            </div>
+                        </el-scrollbar>
+                    </div>
+                </template>
+            </el-skeleton>
+            <el-skeleton class="skelton" animated :count="1" :loading="skeletonLoading">
+                <template #template>
+                    <div class="skelton-img-two">
+                        <div class="" v-for="(item, index) in 15" :key="index">
+                            <el-skeleton-item variant="image" class="skelton-img2-two" />
+                            <el-skeleton-item variant="h3" style="width: 100%; margin-top: 5px;" />
+                        </div>
+                    </div>
+                </template>
+                <template #default>
+                    <div class="song-bottom">
+                        <div class="song-bottom-list">
+                            <div class="song-bottom-item" v-for="(item, index) in songList.playlists" :key="item.id">
+                                <div class="block_item" @click="goToPlayList(item)">
+                                    <PlaylistModule :imgSrc="item.coverImgUrl"></PlaylistModule>
+                                </div>
+                                <div class="text">{{ item.name }}</div>
+                            </div>
 
-            </div>
-        </div>
-
+                        </div>
+                    </div>
+                </template>
+            </el-skeleton>
+        </el-space>
     </div>
 </template>
 
 <script setup lang='ts'>
 import PlaylistModule from '@/components/common/PlaylistModule/index.vue'
 import { getTopPlayList, getTopPlayListTags } from '@/api/radioSation'
-import { onMounted, ref, watch } from 'vue'
+import { onMounted, ref, watch, toRefs } from 'vue'
 import { useRouter } from "vue-router";
-import { useThemeStore } from '@/store/modules/theme'
+import { useThemeStore } from '@/store/modules/theme';
+import useUserStore from '@/store/modules/user';
 
 let songType = ref<string>('全部');
 let songList = ref<any>([]);
@@ -54,6 +93,7 @@ let loading = ref<boolean>(false);
 const router = useRouter();//倒入路由模块
 const musicThemeStore = useThemeStore();
 const isBg = ref<boolean>();
+const { skeletonLoading } = toRefs(useUserStore());
 
 const getPayList = async () => {
     loading.value = true;
@@ -227,6 +267,51 @@ onMounted(async () => {
         background-color: #fff;
     }
 
+
+}
+
+.skelton {
+    .skelton-img {
+        display: flex;
+        padding: 15px;
+        width: 100%;
+
+        .skelton-img2 {
+            height: 144px;
+            width: 144px;
+            border-radius: 10px;
+            margin-right: 20px;
+        }
+
+        .skelton-img1 {
+            flex: 1;
+        }
+    }
+
+    .skelton-box {
+        display: flex;
+        width: 100%;
+        overflow: hidden;
+
+        .skelton-box-item {
+            width: 32px;
+            height: 29px;
+            margin: 10px;
+        }
+    }
+
+    .skelton-img-two {
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(5, minmax(0px, 1fr));
+        gap: 8px 12px;
+
+        .skelton-img2-two {
+            height: 310px;
+            width: 100%;
+        }
+
+    }
 }
 
 ::v-deep .el-loading-spinner {
