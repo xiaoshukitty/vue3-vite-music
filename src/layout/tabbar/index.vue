7b9d2622846f39802706e6b9a6029eb5fdb9f317
xiaoshu <2376628285@qq.com>
2024-09-13 13:19:22 +0800

    fix(layout/tabbar):theme Toggle success

--- src/layout/tabbar/index.vue
+++ src/layout/tabbar/index.vue
@@ -5,8 +5,8 @@
     </div>
     <div class="tabbar_right">
         <LoginClick></LoginClick>
-        <el-switch v-model="dark" class="mt-2" style="margin-left: 24px" inline-prompt :active-icon="Sunny"
-            :inactive-icon="Moon" @change="changeDark" />
+        <el-switch ref="elSwitch" v-model="dark" class="mt-2" style="margin-left: 24px" inline-prompt
+            :active-icon="Sunny" :inactive-icon="Moon" @click="changeDark" />
     </div>
 </template>
 
@@ -36,18 +36,46 @@ if (GET_STORAGE('THEME') == 'dark') {
     dark.value = false;
 }
 
-const changeDark = () => {
+const changeDark = (event: MouseEvent) => {
+    const transition = (document as Document).startViewTransition(() => {
+        //判断标签是否有 dark
+        if (dark.value) {
+            html.className = 'dark';
+            layOutThemeStore.theme = 'dark'
+            SET_STORAGE('THEME', 'dark');
+        } else {
+            html.className = '';
+            layOutThemeStore.theme = 'light';
+            SET_STORAGE('THEME', 'light');
+        }
+    });
 
-    //判断标签是否有 dark
-    if (dark.value) {
-        html.className = 'dark';
-        layOutThemeStore.theme = 'dark'
-        SET_STORAGE('THEME', 'dark');
-    } else {
-        html.className = '';
-        layOutThemeStore.theme = 'light';
-        SET_STORAGE('THEME', 'light');
-    }
+    //开关按钮的坐标
+    const x = event.clientX;
+    const y = event.clientY;
+
+    //计算开关按钮到页面对角的距离(半径)
+    const tragetRadius = Math.hypot(
+        Math.max(x, window.innerWidth - x),
+        Math.max(y, window.innerHeight - y)
+    )
+    console.log('tragetRadius----', tragetRadius);
+
+    transition.ready.then(() => {
+        //过渡动画结束
+        //第一个参数是关键帧，第二个参数是可选项
+        document.documentElement.animate({
+            // 裁剪路径，中心圆点从 0% 到 100% 
+            clipPath: [
+                `circle(0% at ${x}px ${y}px)`,
+                `circle(${tragetRadius}px at  ${x}px ${y}px)`
+            ],
+        }, {
+            duration: 1000,
+            pseudoElement: '::view-transition-new(root)'
+        })
+    })
+
 }
 
 </script>
@@ -56,8 +84,14 @@ const changeDark = () => {
     display: flex;
     align-items: center;
 }
-.tabbar_right{
+
+.tabbar_right {
     display: flex;
     align-items: center;
+}
+
+::view-transition-new(root),
+::view-transition-old(root) {
+    animation: none !important;
 }
 </style>
